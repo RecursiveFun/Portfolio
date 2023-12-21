7cc77ec07ea710fd0494c741d09b7893bcff08e9
Felix <felixthepuppy@yahoo.com>
2023-12-20 22:51:09 -0500

    Finshed with the projects components.

--- Projects.js
+++ Projects.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 export default class Projects extends Component {
     render() {
         return(
@@ -18,7 +17,7 @@ export default class Projects extends Component {
                         <div className="card-content">
                         <iframe className="thumbnail" width="100%" height="auto" src="https://www.youtube.com/embed/td2zmk2rGRk" title="Appointment Scheduler" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"></iframe>
                             <h3>C# WinForms Appointment Scheduler Application</h3>
-                            <p>C# WinForms scheduling application with globalization and connected to a MySQL database hosted locally.</p>
+                            <p>C# WinForms scheduling application with globalization, displaying the appointments in the users local time and converting it to the MYSQL servers time in UTC 00:00 automagically.</p>
                             <a target="_blank" href="https://github.com/RecursiveFun/Appointment_Scheduler"><img width="50px" height ="auto" src="/images/github.png"/>GitHub</a>
                         </div>
                 </div> 
@@ -36,11 +35,12 @@ export default class Projects extends Component {
                 </div> 
                 <div className="card">
                         <picture className="thumbnail">
-                          <img src="/images/photo.jpg"/>
+                          <img src="https://private-user-images.githubusercontent.com/50165092/292078646-8c182df9-e5c9-47a0-8772-c2a7bee06823.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDMxMjk4MjAsIm5iZiI6MTcwMzEyOTUyMCwicGF0aCI6Ii81MDE2NTA5Mi8yOTIwNzg2NDYtOGMxODJkZjktZTVjOS00N2EwLTg3NzItYzJhN2JlZTA2ODIzLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzEyMjElMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMxMjIxVDAzMzIwMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWY5ODUzYWUwOWQxMzYzYmZiNzFiNDBiNTJhODI2OTJlZmQ4NGEzYjExOWNmNGQxNDk0ZGNmOTIzMjE0YTg2NTAmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.hmjLiPZ-MhYEsylhFs8uvFjxC6XmBWwuf09y6BAUZV8"/>
                         </picture>
                         <div className="card-content">
-                            <h3>Project 4</h3>
-                            <p>Project Description</p>
+                            <h3>New Paths Inc. Company Website</h3>
+                            <p>The website was designed with custom HTML, CSS, Javascipt, PHP and also used the bootstrap framework.</p>
+                            <a target="_blank" href="https://github.com/RecursiveFun/NPIWebsite"><img width="50px" height ="auto" src="/images/github.png"/>GitHub</a>
                         </div>
                 </div> 
           </section>
