29a68618177a1dc3fea981fcde9654d8512cd47c
RecursiveFun <felixthepuppy@yahoo.com>
2024-05-24 00:18:38 -0400

    Add SpeedInsights

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 
@@ -24,6 +25,7 @@ export default function Nextra({ Component, pageProps }) {
       </Head>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
